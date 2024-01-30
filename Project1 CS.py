1f2ce3dfe2447e1d5870e0f487cbf7c60640e2e9
VideshSwaminathan <158181261+VideshSwaminathan@users.noreply.github.com>
2024-01-30 20:25:35 +0530

    Add files via upload

---
+++ Project1 CS.py
@@ -0,0 +1,4 @@
+#this is to convert fahernheit into celcius
+a=float(input("type fahernheit:"))
+b=(a-32)*5/9
+print("the celcius is:",b)
---
+++ Project2 CS.py
@@ -0,0 +1,10 @@
+#Traffic light simulation
+a=input("type any colour of the traffic lights in small letters without space:")
+if(a=='green'):
+    print("The colour Green represents to go")
+elif(a=='red'):
+    print("The colour Red represents to stop")
+elif(a=='yellow'):
+    print("The colour Yellow represents to slow down")
+else:
+    print("The colour is INVAILD!")
